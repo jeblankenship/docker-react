05bb2fa2501673fa34e817bbfe7fdc4d579e08fd
James Blankenship <jeblankenship@gmail.com>
2019-02-04 20:31:40 -0500

    Added app text

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Go There!!!
+            New Feature Added
           </p>
           <a
             className="App-link"
